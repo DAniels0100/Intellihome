e9a37f0ecb4cb4a19a96f157ec7bfbd691399fdd
Daniels <se.alchanto@gmail.com>
2024-10-01 23:13:10 -0600

    Conexion con Base de Datos #2

--- IntelliHome/app/src/main/java/com/example/intellihome/Register.java
+++ IntelliHome/app/src/main/java/com/example/intellihome/Register.java
@@ -6,12 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Register extends AppCompatActivity {
+
+    //crear objeto de la DataBaseReference para acceder a la base de datos de FireBase realtime database
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://intellihome-293ec-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +33,10 @@ public class Register extends AppCompatActivity {
 
         final EditText nombre = findViewById(R.id.nombreInput);
         final EditText apellido = findViewById(R.id.apellidoInput);
+        final EditText nickName = findViewById(R.id.nicknameInput);
         final EditText numeroTelefono = findViewById(R.id.numeroTelefonoInput);
         final EditText email = findViewById(R.id.emailInput);
+        final EditText domicilio = findViewById(R.id.domicilioInput);
         final EditText contrasena = findViewById(R.id.contrasenaInput);
         final EditText contrasenaVerificacion = findViewById(R.id.contrasenaVerificacionInput);
         final Button registroBtn = findViewById(R.id.registrarseBtn);
@@ -41,17 +56,88 @@ public class Register extends AppCompatActivity {
         registroBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //recuperar la data del usuario
                 final String nombreTxt = nombre.getText().toString();
                 final String apellidoTxt = apellido.getText().toString();
+                final String nickNameTxt = nickName.getText().toString();
                 final String numeroTelefonoTxt = numeroTelefono.getText().toString();
                 final String emailTxt = email.getText().toString();
+                final String domicilioTxt = domicilio.getText().toString();
                 final String contrasenaTxt = contrasena.getText().toString();
                 final String contrasenaVerificacionTxt = contrasenaVerificacion.getText().toString();
+                final String edadTxt = edad.getText().toString();
+
+                //verificar que se llenaron todas las casillas
+                if(nombreTxt.isEmpty() || apellidoTxt.isEmpty() || numeroTelefonoTxt.isEmpty() ||
+                        emailTxt.isEmpty() || contrasenaTxt.isEmpty() || contrasenaVerificacionTxt.isEmpty() ||
+                        edadTxt.isEmpty()){
+                    Toast.makeText(Register.this,"Se necesitan llenar todas las casillas", Toast.LENGTH_SHORT).show();
+                } else if (isPasswordValid(contrasenaTxt)==false || isPasswordValid(contrasenaVerificacionTxt)==false) {
+                    Toast.makeText(Register.this, "La contraseña debe contener una mayúscula, una minúscula y un caractér especial", Toast.LENGTH_LONG).show();
+                } else if (!contrasenaTxt.equals(contrasenaVerificacionTxt)) {
+                    Toast.makeText(Register.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                }else{
+
+                    databaseReference.child("usuarios").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            //verificar que no exista el usuario
+                            if (snapshot.hasChild(nickNameTxt)){
+                                Toast.makeText(Register.this, "Ya el usuario existe", Toast.LENGTH_SHORT).show();
+                            }else{
+                                //se usa como identificador/raiz el nickname del usuario
+                                //se envia la data a la base de datos
+                                databaseReference.child("usuarios").child(nickNameTxt).child("nombre").setValue(nombreTxt);
+                                databaseReference.child("usuarios").child(nickNameTxt).child("apellido").setValue(apellidoTxt);
+                                databaseReference.child("usuarios").child(nickNameTxt).child("numeroTelefono").setValue(numeroTelefonoTxt);
+                                databaseReference.child("usuarios").child(nickNameTxt).child("email").setValue(emailTxt);
+                                databaseReference.child("usuarios").child(nickNameTxt).child("contrasena").setValue(contrasenaTxt);
+                                databaseReference.child("usuarios").child(nickNameTxt).child("edad").setValue(edadTxt);
+                                databaseReference.child("usuarios").child(nickNameTxt).child("domicilio").setValue(domicilioTxt);
+                                //mostrar mensaje de que se logro guardar la data en la base de datos
+                                Toast.makeText(Register.this, "Se ha registrado de manera exitosa", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
             }
         });
     }
 
+    //verificar que la contrasena es valida
+    public static boolean isPasswordValid(String password) {
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasSpecialChar = false;
 
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                hasUpperCase = true;
+            } else if (Character.isLowerCase(c)) {
+                hasLowerCase = true;
+            } else if (!Character.isLetterOrDigit(c)) {
+                hasSpecialChar = true;
+            }
+
+            // Si ya encontramos las tres condiciones, no necesitamos seguir buscando
+            if (hasUpperCase && hasLowerCase && hasSpecialChar) {
+                return true;
+            }
+        }
+
+        // Si alguna condición no se cumple, la contraseña no es válida
+        return false;
+    }
 
     // Método para mostrar el DatePicker para la edad
     private void showDatePickerDialog(EditText edadEditText) {
@@ -65,7 +151,6 @@ public class Register extends AppCompatActivity {
                 (view, selectedYear, selectedMonth, selectedDay) -> {
                     Calendar selectedCalendar = Calendar.getInstance();
                     selectedCalendar.set(selectedYear, selectedMonth, selectedDay);
-
                     int age = calculateAge(selectedCalendar.getTimeInMillis());
                     edadEditText.setText(String.valueOf(age));
                 },
@@ -136,3 +221,5 @@ public class Register extends AppCompatActivity {
         builder.create().show();
     }
 }
+
+
